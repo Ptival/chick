d57955977824f8e38aa16c58c28aa8e2c9e3960b
Valentin Robert <valentin.robert.42@gmail.com>
2017-03-06 18:53:33 -0800

    implemented substitution based on α-renaming

--- lib/Term/Substitution.hs
+++ lib/Term/Substitution.hs
@@ -1,27 +1,32 @@
 {-# language LambdaCase #-}
+{-# language ScopedTypeVariables #-}
 
 module Term.Substitution where
 
+import Term.AlphaRenaming
+import Term.Free
+import Term.Fresh
 import Term.Term
 
 -- does NOT substitute in annotations!
-subst :: Variable -> TermX ξ -> TermX ξ -> TermX ξ
-subst v s = go
+subst :: forall ξ. Variable -> TermX ξ -> TermX ξ -> TermX ξ
+subst target replacement = go
   where
     go = \case
-      Annot a t τ -> Annot a (go t) (go τ)
-      App a t1 t2 -> App   a (go t1) (go t2)
-      Hole a -> Hole  a
-      Lam a (Binder b) t
-        | b == Just v -> Lam a (Binder b) t
-        | otherwise  -> Lam a (Binder b) (go t)
-      Let a (Binder b) t1 t2
-        | b == Just v -> Let a (Binder b) (go t1) t2
-        | otherwise  -> Let a (Binder b) (go t1) (go t2)
-      Pi a (Binder b) τ1 τ2
-        | b == Just v -> Pi a (Binder b) (go τ1) τ2
-        | otherwise  -> Pi a (Binder b) (go τ1) (go τ2)
-      Type a -> Type a
-      Var a x
-        | x == v     -> s
-        | otherwise -> Var a v
+      Annot a t τ     -> Annot a (go t) (go τ)
+      App   a t1 t2   -> App   a (go t1) (go t2)
+      Hole  a         -> Hole  a
+      Lam   a b t     -> goBound (Lam a) b t
+      Let   a b t1 t2 -> goBound (\ b' t2' -> Let a b' (go t1) t2') b t2
+      Pi    a b τ1 τ2 -> goBound (\ b' τ2' -> Pi  a b' (go τ1) τ2') b τ2
+      Type  a         -> Type  a
+      Var   a v
+        | v == target -> replacement
+        | otherwise  -> Var a v
+    goBound :: (Binder -> TermX ξ -> TermX ξ) -> Binder -> TermX ξ -> TermX ξ
+    goBound k (Binder b) t = case b of
+      Just v | v == target      -> k (Binder b) t
+      Just v | v `elem` freeVars replacement ->
+               let f = freshAvoid ([target] ++ freeVars replacement) t in
+               k (Binder (Just f)) (go (αrename v f t))
+      _                        -> k (Binder b) (go t)
