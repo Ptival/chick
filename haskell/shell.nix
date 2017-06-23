e0d358fb77c6b42ea1b70fd16188cef5fc0f95f9
Valentin Robert <valentin.robert.42@gmail.com>
2017-06-22 21:35:49 -0700

    adding name to derivation

--- haskell/shell.nix
+++ haskell/shell.nix
@@ -1,6 +1,7 @@
 { nixpkgs ? import <nixpkgs> {}, compiler ? "ghc802" }:
 let chick = (import ./default.nix { inherit nixpkgs compiler; }).env; in
 nixpkgs.stdenv.lib.overrideDerivation chick (old: {
+  name = "chick";
   buildInputs = (with nixpkgs; [
     cabal-install
     coq_8_6
