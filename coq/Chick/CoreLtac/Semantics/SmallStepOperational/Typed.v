6d691757487f57cf7863b3d7f1f9c3787506e22f
Valentin Robert <valentin.robert.42@gmail.com>
2017-07-16 18:52:10 -0700

    separated other versions into their own branch

--- coq/Chick/CoreLtac/Semantics/SmallStepOperational/Typed.v
+++
@@ -1,4 +0,0 @@
-From Chick.CoreLtac.Semantics.SmallStepOperational.Typed Require Export
-     Atomic
-     Tactic
-.
--- purescript/src/Bound/Bound.purs
+++
@@ -1,8 +0,0 @@
-module Bound.Bound where
-
-import Prelude
-
-class Bound t where
-  boundSubst :: ∀ f a c. Applicative f => Bind f => t f a -> (a -> f c) -> t f c
-
-infixl 1 boundSubst as >>>=
