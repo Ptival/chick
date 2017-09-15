43ec65a143a532b8be018248d960d87d23f894e8
Valentin Robert <valentin.robert.42@gmail.com>
2017-09-15 15:36:30 -0700

    adding test for δStdLib

--- lib/StandardLibraryDiff.hs
+++ lib/StandardLibraryDiff.hs
@@ -2,11 +2,16 @@
 --{-# LANGUAGE FlexibleContexts #-}
 --{-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE OverloadedStrings #-}
+
 --{-# LANGUAGE ScopedTypeVariables #-}
 
 module StandardLibraryDiff
-  ( δListToVec
-  )where
+  ( δBoolToNat
+  , δListToVec
+  , δNatToList
+  ) where
+
+import Text.Printf
 
 import qualified Diff.Atom as DA
 import qualified Diff.Constructor as DC
@@ -14,8 +19,50 @@ import qualified Diff.Inductive as DI
 import qualified Diff.List as DL
 import qualified Diff.Pair as DP
 import qualified Diff.Term as DT
+import           Parsing
 import           Term.Term
 import qualified Term.Raw as Raw
+
+-- do not use `unsafeParseRaw` anywhere else!
+unsafeParseRaw :: String -> Raw.Term Variable
+unsafeParseRaw s =
+  case parseMaybeTerm s of
+    Nothing -> error $ printf "unsafeParseRaw: could not parse %s" s
+    Just t  -> t
+
+δBoolToNat :: DI.Diff Raw.Raw
+δBoolToNat = DI.Modify δn δps δis δcs
+
+  where
+
+    δn = DA.Replace "ℕ"
+    δps = DL.Same
+    δis = DL.Same
+    -- for sake of testing, let's permute
+    δcs = DL.Permute [1, 0] $ DL.Modify δfalse $ DL.Modify δtrue $ DL.Same
+
+    δfalse = DC.Modify (DA.Replace "zero") DL.Same DL.Same
+    δtrue = DC.Modify (DA.Replace "succ") δsuccPs DL.Same
+
+    δsuccPs = DL.Insert ("n", "ℕ") DL.Same
+
+δNatToList :: DI.Diff Raw.Raw
+δNatToList = DI.Modify δn δps δis δcs
+
+  where
+
+    δn = DA.Replace "List"
+    δps = DL.Insert ("A", Type) DL.Same
+    δis = DL.Same
+    δcs = DL.Modify δzeroToNil $ DL.Modify δsuccToCons $ DL.Same
+
+    δzeroToNil = DC.Modify (DA.Replace "nil") DL.Same DL.Same
+    δsuccToCons = DC.Modify (DA.Replace "cons") δsuccToConsPs DL.Same
+
+    δsuccToConsPs =
+      DL.Insert ("x", "A")
+      $ DL.Modify (DP.Modify (DA.Replace "xs") (DT.Replace (unsafeParseRaw "List A")))
+      $ DL.Same
 
 δListToVec :: DI.Diff Raw.Raw
 δListToVec = DI.Modify δn δps δis δcs
---
+++ test/StandardLibraryDiff/Test.hs
@@ -0,0 +1,32 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+
+module StandardLibraryDiff.Test where
+
+import Control.Monad.Freer.Exception
+
+import qualified Diff.Inductive as DI
+import Inductive.Inductive
+import StandardLibrary
+import StandardLibraryDiff
+import Term.Term
+import Utils
+
+test ::
+  Show α =>
+  Inductive α Variable -> DI.Diff α -> Inductive α Variable -> IO Bool
+test indFrom δind indTo = do
+  case skipTrace . runError $ DI.patch indFrom δind of
+    Left (e :: String) -> do
+      putStrLn e
+      return False
+    Right indTo' ->
+      return $ indTo == indTo'
+
+testδBoolToNat :: IO Bool
+testδBoolToNat = test indBool δBoolToNat indNat
+
+testδNatToList :: IO Bool
+testδNatToList = test indNat δNatToList indList
+
+testδListToVec :: IO Bool
+testδListToVec = test indList δListToVec indVec
