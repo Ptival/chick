22ec796a10803960fe63bc5dc5cf572cd5fde72e
Valentin Robert <valentin.robert.42@gmail.com>
2017-09-16 14:10:12 -0700

    added tests for δeliminator

--- test/Diff/Eliminator/Test.hs
+++ test/Diff/Eliminator/Test.hs
@@ -1,47 +1,107 @@
+{-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE UnicodeSyntax #-}
 
 module Diff.Eliminator.Test where
 
+import           Control.Monad
+import           Control.Monad.Freer
 import           Control.Monad.Freer.Exception
 import           Control.Monad.Freer.Trace
+import           System.Exit
+import           Text.Printf
 
 import           Diff.Eliminator
-import           Diff.Motive
+import qualified Diff.Inductive as DI
 import qualified Diff.Term as DT
 import           Inductive.Eliminator
-import           Inductive.Motive
+import           Inductive.Inductive
 import           PrettyPrinting.PrettyPrintable
 import           StandardLibrary
 import           StandardLibraryDiff
 import           Term.Term
 import           Utils
 
-testListToVec :: IO Bool
-testListToVec =
-  let listEliminator = mkEliminatorType () indList in
-  let  vecEliminator = mkEliminatorType () indVec  in
-  case δmkEliminatorType () indList δListToVec of
+test ::
+  ( Member (Exc String) r
+  , Member Trace r
+  ) =>
+  Inductive () Variable -> DI.Diff () -> Inductive () Variable -> Eff r Bool
+test indFrom δind indTo =
+  let elimFrom = mkEliminatorType () indFrom in
+  let elimTo   = mkEliminatorType () indTo   in
+  case δmkEliminatorType () indFrom δind of
+
   Nothing -> do
-    putStrLn "δmkEliminatorType failed"
+    trace "δmkEliminatorType failed"
     return False
-  Just δlistEliminator -> do
-    putStrLn $ "Eliminator before:"
-    putStrLn $ prettyStr listEliminator
-    putStrLn $ replicate 80 '-'
-    putStrLn $ "δ computed to patch eliminator:"
-    putStrLn $ prettyStr δlistEliminator
-    putStrLn $ replicate 80 '-'
-    putStrLn $ "Eliminator expected:"
-    putStrLn $ prettyStr vecEliminator
-    putStrLn $ replicate 80 '-'
-    (runSkipTrace . runError $ DT.patch listEliminator δlistEliminator) >>= \case
-      Left (s :: String) -> do
-        putStrLn "An error occurred:"
-        putStrLn s
-        return False
-      Right vecEliminator' -> do
-        putStrLn $ "Eliminator obtained:"
-        putStrLn $ prettyStr vecEliminator'
-        putStrLn $ "Eliminator expected and obtained are α-equivalent?"
-        return (vecEliminator' == vecEliminator)
+
+  Just δelim -> do
+    trace $ "-- Eliminator before:"
+    trace $ prettyStr elimFrom
+    trace $ replicate 80 '-'
+    trace $ "-- δ computed to patch eliminator:"
+    trace $ prettyStr δelim
+    trace $ replicate 80 '-'
+    trace $ "-- Eliminator expected:"
+    trace $ prettyStr elimTo
+    trace $ replicate 80 '-'
+    elimTo' <- DT.patch elimFrom δelim
+    trace $ "-- Eliminator obtained:"
+    trace $ prettyStr elimTo'
+    trace $ replicate 80 '-'
+    trace $ "-- Eliminator expected and obtained are α-equivalent?"
+    return (elimTo' == elimTo)
+
+type Test r =
+  ( Member (Exc String) r
+  , Member Trace r
+  ) => Eff r Bool
+
+testδBoolToNat ::
+  ( Member (Exc String) r
+  , Member Trace r
+  ) =>
+  Eff r Bool
+testδBoolToNat = test indBool δBoolToNat indNat
+
+testδNatToList ::
+  ( Member (Exc String) r
+  , Member Trace r
+  ) =>
+  Eff r Bool
+testδNatToList = test indNat δNatToList indList
+
+testδListToVec ::
+  ( Member (Exc String) r
+  , Member Trace r
+  ) =>
+  Eff r Bool
+testδListToVec = test indList δListToVec indVec
+
+tests ::
+  ( Member (Exc String) r
+  , Member Trace r
+  ) =>
+  [(String, Eff r Bool)]
+tests =
+  [ ("testδBoolToNat", testδBoolToNat)
+  , ("testδNatToList", testδNatToList)
+  , ("testδListToVec", testδListToVec)
+  ]
+
+main :: IO ()
+main = do
+  putStrLn "\n"
+  forM_ tests $ \ (name, t) -> do
+    (runSkipTrace $ runError t) >>= \case
+      Right True -> putStrLn $ printf "[✓] %s" name
+      Right False -> do
+        putStrLn $ printf "[✗] %s: patching succeeded, but unexpected result" name
+        exitFailure
+      Left (err :: String) -> do
+        putStrLn $ printf "[✗] %s: %s" name err
+        exitFailure
+  exitSuccess
