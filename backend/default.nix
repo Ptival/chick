53a6a31b84e698b1b076cc28fa64df0e39b6b64d
Valentin Robert <valentin.robert.42@gmail.com>
2020-08-03 23:24:48 +0200

    haskell.nix setup

--- backend/default.nix
+++ backend/default.nix
@@ -1,6 +1,54 @@
-# { nur ? (import <nixpkgs> {}).nur.repos.ptival
-{ nur ? (import ~/personal/nur-packages {})
-, nixpkgs ? import <nixpkgs> { overlays = [ nur.overlays.chick ]; }
-}:
-with nixpkgs;
-haskell.lib.doCheck (haskellPackages.callCabal2nix "chick" ./. {})
+let
+
+  name = "chick";
+  compiler-nix-name = "ghc884";
+
+  sources = import ./nix/sources.nix {};
+  haskellNix = import (fetchTarball { inherit (sources."haskell.nix") url sha256; }) {
+      sourcesOverride = {
+        hackageSrc = fetchTarball { inherit (sources."hackage.nix") url sha256; };
+      };
+  };
+
+  pkgs = import sources.nixpkgs haskellNix.nixpkgsArgs;
+
+  project = pkgs.haskell-nix.cabalProject {
+
+    inherit compiler-nix-name;
+
+    src = pkgs.haskell-nix.haskellLib.cleanGit {
+      inherit name;
+      src = ../.;
+      subDir = "backend";
+    };
+
+  };
+
+in
+
+project.${name}.components.library // {
+
+  shell = project.shellFor {
+
+    buildInputs =
+      let
+        hsPkgs = pkgs.haskell.packages.${compiler-nix-name};
+      in [
+        hsPkgs.haskell-language-server
+      ];
+
+    packages = p: [
+      p.chick
+      p.language-ocaml
+    ];
+
+    tools = {
+      cabal = "3.2.0.0";
+      hlint = "2.2.11";
+      hpack = "0.34.2";
+      ormolu = "0.1.2.0";
+    };
+
+  };
+
+}
--- backend/shell.nix
+++ backend/shell.nix
@@ -1,14 +1,1 @@
-{ nur ? (import ~/personal/nur-packages {})
-, nixpkgs ? import <nixpkgs> { overlays = [ nur.overlays.chick ]; }
-}:
-with nixpkgs;
-mkShell {
-  buildInputs = [
-    cabal-install
-    haskellPackages.hlint
-  ];
-  inputsFrom = [
-    (haskellPackages.callCabal2nix "chick" ./. {}).env
-  ];
-  name = "chick";
-}
+(import ./.).shell
