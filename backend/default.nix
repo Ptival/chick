b9951385c56315e6e0d6da4e761e11a16a106dfe
Valentin Robert <valentin.robert.42@gmail.com>
2020-10-13 21:09:04 +0200

    working haskell.nix + haskell-language-server

--- backend/default.nix
+++ backend/default.nix
@@ -8,13 +8,37 @@ let
   haskellNix = import (fetchNiv "haskell.nix") {
     sourceOverrides = import (fetchNiv "hackage.nix");
   };
-  all-hies = import (fetchNiv "all-hies") {};
 
   pkgs = import haskellNix.sources.nixpkgs-2003 (haskellNix.nixpkgsArgs // {
     overlays = haskellNix.nixpkgsArgs.overlays ++ [
-      all-hies.overlay
     ];
   });
+
+  hls-set = pkgs.haskell-nix.cabalProject {
+    src = pkgs.fetchFromGitHub {
+      name = "haskell-language-server";
+      inherit (sources.haskell-language-server) owner repo rev;
+      # owner = "haskell";
+      # repo = "haskell-language-server";
+      # rev = "0.5.0";
+      # Need to override the hash due to lack of niv submodule support
+      sha256 = "0vkh5ff6l5wr4450xmbki3cfhlwf041fjaalnwmj7zskd72s9p7p";
+      fetchSubmodules = true;
+    };
+    # src = fetchNiv "haskell-language-server";
+    lookupSha256 = { location, tag, ... } : {
+      "https://github.com/bubba/brittany.git"."c59655f10d5ad295c2481537fc8abf0a297d9d1c" = "1rkk09f8750qykrmkqfqbh44dbx1p8aq1caznxxlw8zqfvx39cxl";
+    }."${location}"."${tag}";
+    inherit compiler-nix-name; # index-state; # checkMaterialization;
+    # Plan issues with the benchmarks, can try removing later
+    configureArgs = "--disable-benchmarks";
+    # Invalidate and update if you change the version
+    plan-sha256 = "0ga7a2d22hbxvhx4363g0iyss4x5kaamkxh7bhmcki6azsha92vz";
+    modules = [{
+      # Tests don't pass for some reason, but this is a somewhat random revision.
+      packages.haskell-language-server.doCheck = false;
+    }];
+  };
 
   set = pkgs.haskell-nix.cabalProject {
 
@@ -60,6 +84,15 @@ let
 
     ];
 
+    pkg-def-extras = [
+      (hackage: {
+        packages = {
+          fourmolu = hackage.fourmolu."0.1.0.0".revisions.default;
+          shake = hackage.shake."0.18.4".revisions.default;
+        };
+      })
+    ];
+
     src = pkgs.haskell-nix.haskellLib.cleanGit {
       inherit name;
       src = ./.;
@@ -73,23 +106,24 @@ set.${name}.components.library // {
 
   shell = set.shellFor {
 
-    # exactDeps = true;
+    buildInputs =
+      [
+        hls-set.haskell-language-server.components.exes.haskell-language-server
+        set.happy.components.exes.happy
+      ];
+
+    exactDeps = false;
 
     packages = p: [
       p.${name}
       p.language-ocaml
     ];
 
-    shellHook = ''
-      export HIE_HOOGLE_DATABASE=$(realpath "$(dirname "$(realpath "$(which hoogle)")")/../share/doc/hoogle/default.hoo")
-    '';
-
     tools = {
       cabal = {
-        inherit (nix-compiler-name);
+        inherit compiler-nix-name;
         version = "3.2.0.0";
       };
-      hie = "unstable";
       hlint = "2.2.11";
       hpack = "0.34.2";
       ormolu = "0.1.2.0";
